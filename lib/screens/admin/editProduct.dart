bc790c29de3b8b71738c69e07ecc50ba04d7f111
AhmedAdly20 <ahmedadly132@gmail.com>
2020-08-04 01:18:13 +0200

    AdminProducts Page UI

--- lib/screens/admin/editProduct.dart
+++ lib/screens/admin/editProduct.dart
@@ -17,29 +17,92 @@ class _EditProductState extends State<EditProduct> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: StreamBuilder<QuerySnapshot>(
-        stream: _store.loadProducts(),
-        builder:(context , snapshot) {
-          if (snapshot.hasData) {
-            List<Product> products= [];
-            for (var doc in snapshot.data.documents) {
-              var data = doc.data;
-              products.add(Product(
-                pCategory: data[kProductCategory],
-                pDescription: data[kProductDescription],
-                pLocation: data[kProductLocation],
-                pName: data[kProductName],
-                pPrice: data[kProductPrice],
-              ));
+          stream: _store.loadProducts(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              List<Product> products = [];
+              for (var doc in snapshot.data.documents) {
+                var data = doc.data;
+                products.add(Product(
+                  pCategory: data[kProductCategory],
+                  pDescription: data[kProductDescription],
+                  pLocation: data[kProductLocation],
+                  pName: data[kProductName],
+                  pPrice: data[kProductPrice],
+                ));
+              }
+              return GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2, childAspectRatio: 0.8),
+                itemBuilder: (context, index) => Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  child: GestureDetector(
+                    onTapUp: (details){
+                      double dx = details.globalPosition.dx;
+                      double dy = details.globalPosition.dy;
+                      double dx2 = MediaQuery.of(context).size.width - dx;
+                      double dy2 = MediaQuery.of(context).size.height - dy;
+                      showMenu(
+                        context: context,
+                        position: RelativeRect.fromLTRB(dx, dy, dx2, dy2),
+                        items: [
+                          PopupMenuItem(
+                            child: Text('Edit'),
+                          ),
+                          PopupMenuItem(
+                            child: Text('Delete'),
+                          ),
+                        ]
+                      );
+                    },
+                    child: Stack(
+                      children: <Widget>[
+                        Positioned.fill(
+                          child: Image(
+                            image: AssetImage(products[index].pLocation),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Positioned(
+                          bottom: 0,
+                          child: Opacity(
+                            opacity: 0.6,
+                            child: Container(
+                              width: MediaQuery.of(context).size.width,
+                              height: 60.0,
+                              color: Colors.white,
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 10.0, vertical: 5),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(
+                                      products[index].pName,
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    Text(
+                                      '\$' + products[index].pPrice,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                itemCount: products.length,
+              );
+            } else {
+              return Center(child: CircularProgressIndicator());
             }
-            return ListView.builder(
-            itemBuilder: (context , index) => Text(products[index].pName),
-            itemCount: products.length,
-            );
-          } else {
-            return Center(child: CircularProgressIndicator());
-          }
-        }
-      ),
+          }),
     );
   }
 }
